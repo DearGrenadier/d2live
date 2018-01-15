067cd4cac515485d2ee19617d6da7936636a5aab
Dima Nazarchuk <dmitry.nazarchuk@kandasoft.com>
2018-01-16 02:22:15 +0300

    Add matches count to stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,7 @@
 class StatsController < ApplicationController
   def index
     @chats_count = Chat.count
+    @matches_count = Match.count
     @live_matches = Match.live
     @finished_matches = Match.finished.last(10)
   end
