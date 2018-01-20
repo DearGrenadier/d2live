b41029547a4e5f0e2a0ccb9f0be375d4a87fb57c
Dima Nazarchuk <dmitry.nazarchuk@kandasoft.com>
2018-01-20 16:56:45 +0300

    Fix up match register issue

--- app/workers/register_new_match_worker.rb
+++ app/workers/register_new_match_worker.rb
@@ -4,5 +4,6 @@ class RegisterNewMatchWorker
   def perform(match_raw)
     match = Match.create!(id: match_raw.delete('match_id'), raw: match_raw)
     MatchStartNotification.new(match).send
+  rescue
   end
 end
