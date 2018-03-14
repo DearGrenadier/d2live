a00134be5cb9b8dd9c17075e31f3509890697bc7
Dima Nazarchuk <dmitry.nazarchuk@kandasoft.com>
2018-03-14 19:50:28 +0300

    Fix up teams id fetching in RegisterNewMatchWorker

--- app/workers/register_new_match_worker.rb
+++ app/workers/register_new_match_worker.rb
@@ -5,7 +5,7 @@ class RegisterNewMatchWorker
     match_id = match_raw.delete('match_id')
     unless Match.exists? match_id
       match = Match.create!(id: match_id, raw: match_raw)
-      [%w[radiant_team dire_team].map { |side| match_raw.dig(side,'team_id') }].each do |team_id|
+      %w[radiant_team dire_team].map { |side| match_raw.dig(side,'team_id') }.each do |team_id|
         RegisterTeamWorker.new.perform(team_id) unless Team.exists? team_id
       end
       MatchStartNotification.new(match).send if match.consistent?
