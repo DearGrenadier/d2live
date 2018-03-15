cd9e0912bf82f7f7b419b05932eb31667b7a1376
Dima Nazarchuk <dmitry.nazarchuk@kandasoft.com>
2018-03-16 00:24:48 +0300

    Fix up method name typo `recreate_versions!`

--- db/migrate/20180315171340_recreate_team_logo_versions.rb
+++ db/migrate/20180315171340_recreate_team_logo_versions.rb
@@ -1,7 +1,7 @@
 class RecreateTeamLogoVersions < ActiveRecord::Migration[5.1]
   def change
     Team.find_each do |team|
-      team.logo.recreate_versions if team.logo?
+      team.logo.recreate_versions! if team.logo?
     end
   end
 end
