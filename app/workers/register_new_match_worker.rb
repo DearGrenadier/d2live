98ab05233f9cf33cb1fd99063e8594e6ce1f968b
Dima Nazarchuk <dmitry.nazarchuk.work@gmail.com>
2018-11-23 14:31:38 +0300

    Deploy worker container separately (#5)

--- app/workers/register_new_match_worker.rb
+++ app/workers/register_new_match_worker.rb
@@ -5,6 +5,8 @@ class RegisterNewMatchWorker
 
   def perform(match_raw)
     match_id = match_raw.delete('match_id')
+    return if Match.exists?(match_id)
+
     logger.info "Register new match ##{match_id}"
     Match.create!(id: match_id, raw: match_raw)
     # %w[radiant_team dire_team].map { |side| match_raw.dig(side, 'team_id') }.each do |team_id|
--- app/workers/track_matches_worker.rb
+++ app/workers/track_matches_worker.rb
@@ -10,9 +10,8 @@ class TrackMatchesWorker
     json = JSON.parse response.body
     league_matches = json.reject { |match| match['league_id'].zero? }
     league_match_ids = league_matches.map do |league_match|
-      match_id = league_match['match_id']
-      RegisterNewMatchWorker.perform_async(league_match) unless Match.exists?(match_id)
-      match_id
+      RegisterNewMatchWorker.perform_async(league_match)
+      league_match['match_id']
     end
     register_finished_matches(league_match_ids)
   end
