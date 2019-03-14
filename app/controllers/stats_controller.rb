543a413a3347e0f6492940aaea0803971b0c09d7
Dima Nazarchuk <dmitry.nazarchuk.work@gmail.com>
2019-03-14 15:22:50 +0300

    Update start/finish matches information workers (#11)

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,7 +4,6 @@ class StatsController < ApplicationController
   def index
     @chats_count = Chat.count
     @matches_count = Match.count
-    @leagues_count = League.count
     @live_matches = Match.live
     @finished_matches = Match.finished.last(10)
     @last_commit_message = `git log -1 --pretty=%B`
