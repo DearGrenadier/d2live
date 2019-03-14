33d77b980fe1618e496f0249025b2a4932731436
Dima Nazarchuk <dmitry.nazarchuk.work@gmail.com>
2019-03-14 15:08:41 +0300

    Update start/finish matches information workers

--- app/workers/track_new_matches_worker.rb
+++ app/workers/track_new_matches_worker.rb
@@ -6,11 +6,18 @@ class TrackNewMatchesWorker
   include Sidekiq::Worker
 
   def perform
+    logger = Logger.new("#{Rails.root}/log/#{Rails.env}_#{self.class.name.underscore}.log")
+    logger.info '----- Performing track new matches job -----'
     response = Faraday.get('https://api.opendota.com/api/live')
     data = JSON.parse response.body
-    league_matches = data.reject { |match| match['league_id'].zero? }
-    league_matches.each do |league_match|
-      RegisterNewMatchWorker.perform_async(league_match)
+    saved_league_ids = League.pluck(:id)
+
+    data.select do |match_raw|
+      saved_league_ids.include? match_raw.fetch('league_id') && !Match.exists?(match_raw.fetch('match_id'))
+    end.each do |match_raw|
+      match_id = match_raw.delete('match_id')
+      Match.create!(id: match_id, raw: match_raw)
+      logger.info "New match <<#{match_id}>> was saved"
     end
   end
 end
