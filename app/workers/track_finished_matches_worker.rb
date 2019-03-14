2d4610779f0bd021a8251e41a4c1bc643cfc9e43
Dima Nazarchuk <dmitry.nazarchuk.work@gmail.com>
2019-03-14 15:20:28 +0300

    Revert "Update start/finish matches information workers (#9)" (#10)
    This reverts commit 3d189b68d70c1bf4b0676d4ee18c785fba7314fb.

--- app/workers/track_finished_matches_worker.rb
+++ app/workers/track_finished_matches_worker.rb
@@ -6,16 +6,12 @@ class TrackFinishedMatchesWorker
   include Sidekiq::Worker
 
   def perform
-    logger = Logger.new("#{Rails.root}/log/#{Rails.env}_#{self.class.name.underscore}.log")
-    logger.info '----- Performing track finished matches job -----'
-
     Match.live.each do |match|
       response = Faraday.get("https://api.opendota.com/api/match/#{match.id}")
-      next if response.status == '404'
+      json = JSON.parse response.body
+      next if json.key? 'error'
 
-      data = JSON.parse response.body
-
-      logger.info "Match <<#{match.id}>> is finished"
+      logger.info "Register finished match ##{match.id}"
       match.update!(finished: true)
       # MatchEndNotification.new(match).send
     end
